761f839798bc3d2c9ce39ead05b3be02ed672e58
Dhatu Kertayuga <dhatukertayuga@gmail.com>
2020-07-01 20:35:19 +0700

    fixing errror logs

--- newsrunner.py
+++ newsrunner.py
@@ -1,5 +1,10 @@
-import re
-import selenium
+from Lib.newsparser import newsParserData, newsParsing
+
+def main():
+    news = newsParsing()
+    news.run()
+    del news
 
 
-
+if __name__ == '__main__':
+    main()
